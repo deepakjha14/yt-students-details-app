560f77256fdad33ff0efe0d02973dc0d2e54470e
Deepak Jha <deepakjha14.ex@gmail.com>
2024-06-22 22:42:19 +0530

    built in control flow implemented, redirectto function implemented for not found route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from "./auth/login/login.component";
 import { authGuardGuard } from "./auth/auth-guard.guard";
+import { NotFoundComponent } from './dashboard/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,20 @@ export const routes: Routes = [
         path: 'dashboard',
         loadChildren: () => import('./dashboard/dashboard.module').then(m=>m.DashboardModule)
         // canMatch: [authGuardGuard]
+    },
+    {
+        path: "**",
+        redirectTo: ({url}) => {
+            console.log(url);
+            if (url[0].path === "dashboard") {
+                return "dashboard";
+            }
+
+            return 'notfound';
+        }
+    },
+    {
+        path: 'notfound',
+        component: NotFoundComponent
     }
 ];
---
+++ src/app/dashboard/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'students-details-not-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss'
+})
+export class NotFoundComponent {
+
+}
