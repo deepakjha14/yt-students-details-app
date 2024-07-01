905cc8c35602cdf34a50eaed1eaff5feadacd0dd
Deepak Jha <deepakjha14.ex@gmail.com>
2024-07-01 21:03:28 +0530

    Chapter 4 and Chapter 3 covered

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 import { AuthService } from "./auth/auth.service";
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         provideExperimentalZonelessChangeDetection(),
-        AuthService
+        AuthService, 
+        provideClientHydration(withEventReplay())
     ]
 };
